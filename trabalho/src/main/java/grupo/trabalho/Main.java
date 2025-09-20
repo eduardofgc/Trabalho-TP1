de05018041d9204bdf77ed6d5967003bd5961ed7
Eduardo Rocha <dudurbiagini@gmail.com>
2025-09-20 13:29:21 -0300

    solved intellij issues

--- trabalho/src/main/java/grupo/trabalho/Main.java
+++ trabalho/src/main/java/grupo/trabalho/Main.java
@@ -1,6 +1,7 @@
 package grupo.trabalho;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -9,10 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Hello JavaFX!");
-        Scene scene = new Scene(label, 400, 200);
+        Group root = new Group();
+        Scene scene = new Scene(root);
 
-        primaryStage.setTitle("My JavaFX App");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- trabalho/src/main/java/module-info.java
+++ trabalho/src/main/java/module-info.java
@@ -3,14 +3,6 @@ module grupo.trabalho {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
     opens grupo.trabalho to javafx.fxml;   // allow FXML reflection
     exports grupo.trabalho;                // export package to other modules
 }
