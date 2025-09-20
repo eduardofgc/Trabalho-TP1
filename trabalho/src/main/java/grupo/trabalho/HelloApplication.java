72a79ff86b3fd7c6e4fa5f3cefbc1e21c2cd5f25
Eduardo Rocha <dudurbiagini@gmail.com>
2025-09-20 17:39:17 -0300

    janela de login + misc

--- trabalho/src/main/java/grupo/trabalho/HelloApplication.java
+++ trabalho/src/main/java/grupo/trabalho/HelloApplication.java
@@ -4,17 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.scene.Parent;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Gestão de RH");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- trabalho/src/main/java/grupo/trabalho/HelloController.java
+++ trabalho/src/main/java/grupo/trabalho/HelloController.java
@@ -1,14 +1,27 @@
 package grupo.trabalho;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private void handleLogin() {
+        Stage prevStage = (Stage) loginButton.getScene().getWindow();
+        prevStage.close();
+
+
+
     }
 }
