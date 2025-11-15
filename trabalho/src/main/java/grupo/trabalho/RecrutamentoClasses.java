f88cbc2afcd119130bc22db96f2c2fee14482fed
Lucas Centurion <lcenturionnetto@gmail.com>
2025-11-15 11:17:08 -0300

    comentei no RecrutamentoClasses

--- trabalho/src/main/java/grupo/trabalho/RecrutamentoClasses.java
+++ trabalho/src/main/java/grupo/trabalho/RecrutamentoClasses.java
@@ -12,21 +12,43 @@ import java.util.stream.Collectors;
 
 public class RecrutamentoClasses {
 
+    /*
+       ENCAPSULAMENTO PARCIAL:
+       As listas são públicas, o que expõe os dados.
+       (Bom para aprender, mas não ideal em sistemas reais).
+    */
     public static ArrayList<Vaga> vagasArray = new ArrayList<>();
     public static ArrayList<Entrevista> entrevistasArray = new ArrayList<>();
 
+    /*
+       COMPOSIÇÃO:
+       A classe RecrutamentoClasses "contém" objetos do tipo Vaga.
+       A lista não existe sem esta classe.
+    */
     public static void addToVagaList(Vaga minhaVaga){
         vagasArray.add(minhaVaga);
     }
 
+    /*
+       COMPOSIÇÃO + MANIPULAÇÃO DE OBJETOS:
+       A classe controla quais objetos vivem dentro da lista.
+    */
     public static void removeFromVagaList(Vaga minhaVaga){
         vagasArray.remove(minhaVaga);
     }
 
+    /*
+       COMPOSIÇÃO:
+       Armazena objetos da classe Entrevista dentro de uma lista.
+    */
     public static void addToEntrevistaList(Entrevista minhaEntrevista){
         entrevistasArray.add(minhaEntrevista);
     }
 
+    /*
+        COMPOSIÇÃO:
+        A classe gerencia o ciclo de vida dos objetos Entrevista.
+    */
     public static void removeFromEntrevistaList(Entrevista minhaEntrevista){
         entrevistasArray.remove(minhaEntrevista);
     }
