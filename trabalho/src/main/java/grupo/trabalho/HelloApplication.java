a6014f100023f2ce7ceb01880f090f4bc21b2c81
Eduardo Rocha <dudurbiagini@gmail.com>
2025-11-24 11:18:54 -0300

    +mudanças cosmeticas

--- trabalho/src/main/java/grupo/trabalho/HelloApplication.java
+++ trabalho/src/main/java/grupo/trabalho/HelloApplication.java
@@ -7,6 +7,14 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+/*
+TALVEZ no meu pc dê um erro do intelliJ na hora de iniciar envolvendo o javafx controls
+Nesse caso, substituir o module path POR
+
+--module-path "C:\Users\dudur\Downloads\openjfx-25.0.1_windows-x64_bin-sdk\javafx-sdk-25.0.1\lib"
+--add-modules javafx.controls,javafx.fxml
+ */
+
 public class HelloApplication extends Application {
 
     @Override
