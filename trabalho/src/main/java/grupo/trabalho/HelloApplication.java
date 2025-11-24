5a205304cd32d535bb74bee5f902cdef55ae23d7
Eduardo Rocha <dudurbiagini@gmail.com>
2025-11-24 19:18:19 -0300

    ultimo commit ultima entrega

--- trabalho/src/main/java/grupo/trabalho/HelloApplication.java
+++ trabalho/src/main/java/grupo/trabalho/HelloApplication.java
@@ -13,6 +13,9 @@ Nesse caso, substituir o module path POR
 
 --module-path "C:\Users\dudur\Downloads\openjfx-25.0.1_windows-x64_bin-sdk\javafx-sdk-25.0.1\lib"
 --add-modules javafx.controls,javafx.fxml
+
+ALÉM DISSO, lembrar de mudar os caminhos em CadastroFunc e FinanceiroController para "dados_Funcionarios.txt" pra mim
+(original é "trabalho/dados_Funcionarios.txt")
  */
 
 public class HelloApplication extends Application {
